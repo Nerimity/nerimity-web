c4bb4f521437b6808137bb75e50964e6197f40b6
Supertiger <saawalkhn@googlemail.com>
2022-07-15 17:59:38 +0100

    feat: :sparkles: Show save button on edit
    When Updating the server settings, Save button now shows.

--- src/components/ServerSettingsGeneral/ServerSettingsGeneral.tsx
+++ src/components/ServerSettingsGeneral/ServerSettingsGeneral.tsx
@@ -3,37 +3,43 @@ import RouterEndpoints from '../../common/RouterEndpoints';
 import env from '../../common/env';
 import { classNames, conditionalClass } from '../../common/classNames';
 import { useParams } from 'solid-app-router';
-import { createEffect, createSignal, For, JSX, Show } from 'solid-js';
+import { createEffect, createSignal, JSX, Show } from 'solid-js';
 import useStore from '../../chat-api/store/useStore';
 import { useWindowProperties } from '../../common/useWindowProperties';
 import CustomInput from '../CustomInput';
 import DropDown from '../DropDown';
 import Icon from '../Icon';
+import { createStore, reconcile } from 'solid-js/store';
+import CustomButton from '../CustomButton';
+import { createUpdatedSignal } from '../../common/createUpdatedSignal';
+
+
 
 export default function ServerSettingsInvite() {
   const {serverId} = useParams();
   const {tabs, servers, channels} = useStore();
   const windowProperties = useWindowProperties();
   const [mobileSize, isMobileSize] = createSignal(false);
+  const [requestSent, setRequestSent] = createSignal(false);
 
   const server = () => servers.get(serverId);
+
+  const defaultInput = () => ({
+    name: server()?.name || '',
+    defaultChannel: server()?.defaultChannel || '',
+  })
+
+  const [inputValues, updatedInputValues, setInputValue] = createUpdatedSignal(defaultInput());
+
 
   const dropDownChannels = () => channels.getChannelsByServerId(serverId).map(channel => ({
     id: channel._id,
     label: channel.name,
+    onClick: () => {
+      setInputValue('defaultChannel', channel._id);
+    }
   }));
-  
 
-
-  const [inputFields, setInputFields] = createSignal({
-    name: '',
-  })
-
-  createEffect(() => {
-    setInputFields({
-      name: server()?.name || '',
-    })
-  })
 
   createEffect(() => {
     const isMobile = windowProperties.paneWidth()! < env.MOBILE_WIDTH;
@@ -51,19 +57,27 @@ export default function ServerSettingsInvite() {
   })
 
 
+
+  const onSaveButtonClicked = () => {
+    if (requestSent()) return;
+    setRequestSent(true);
+    console.log( updatedInputValues() )
+  }
+
   return (
     <div class={classNames(styles.generalPane, conditionalClass(mobileSize(), styles.mobile))}>
       <div class={styles.title}>Server General</div>
       
-
-
       <Block icon='edit' label='Server Name'>
-        <CustomInput value={inputFields().name} onText={(v) => setInputFields({...inputFields(), name: v}) } />
+        <CustomInput value={inputValues().name} onText={(v) => setInputValue('name', v) } />
       </Block>
       <Block icon='tag' label='Default Channel' description='New members will be directed to this channel.'>
-        <DropDown items={dropDownChannels()} selectedId={server()?.defaultChannel} />
+        <DropDown items={dropDownChannels()} selectedId={inputValues().defaultChannel} />
       </Block>
 
+      <Show when={Object.keys(updatedInputValues()).length}>
+        <CustomButton iconName='save' label='Save Changes' class={styles.saveButton} onClick={onSaveButtonClicked} />
+      </Show>
 
     </div>
   )
@@ -93,3 +107,5 @@ function Block(props: BlockProps) {
     </div>
   )
 }
+
+
