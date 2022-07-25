a222140480c633bde55eafb87edaac74acb554e9
Supertiger <saawalkhn@googlemail.com>
2022-07-25 11:22:52 +0100

    feat: :construction: More progress with server channels settings
    Channels setting now displays all channels.

--- src/components/servers/settings/channels/index.tsx
+++ src/components/servers/settings/channels/index.tsx
@@ -1,10 +1,45 @@
 import styles from './styles.module.scss'
 import RouterEndpoints from '@/common/RouterEndpoints';
-import { useParams } from 'solid-app-router';
-import { onMount} from 'solid-js';
+import { Link, useParams } from 'solid-app-router';
+import { For, onMount} from 'solid-js';
 import useStore from '@/chat-api/store/useStore';
 import SettingsBlock from '@/components/ui/settings-block';
 import Button from '@/components/ui/button';
+import { Channel } from '@/chat-api/store/useChannels';
+import Icon from '@/components/ui/icon';
+
+
+
+function ChannelItem( props: {channel: Channel}) {
+  const { serverId } = useParams();
+  console.log(props.channel)
+
+  const link = RouterEndpoints.SERVER_SETTINGS_CHANNEL(serverId, props.channel._id);
+
+  return (
+    <Link href={link} class={styles.channelItem}>
+      <Icon name='storage' size={18}/>
+      <div class={styles.name}>{props.channel.name}</div>
+      <Icon name='navigate_next' />
+    </Link>
+  )
+}
+
+
+function ChannelList() {
+  const { serverId } = useParams();
+  const { channels } = useStore();
+  const serverChannels = channels.getChannelsByServerId(serverId)
+
+  return (
+    <div class={styles.channelList}>
+      <For each={serverChannels}>
+        {channel => <ChannelItem channel={channel} />}
+      </For>
+    </div>
+  )
+}
+
 
 
 
@@ -29,6 +64,7 @@ export default function ServerSettingsChannel() {
       <SettingsBlock label='Add a new channel' icon='add'>
         <Button label='Add Channel' />
       </SettingsBlock>
+      <ChannelList />
     </div>
   )
 }
