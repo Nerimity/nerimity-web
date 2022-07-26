cb080da6b05ab1d630b36a5f121ac6dc3b85b6db
Supertiger <saawalkhn@googlemail.com>
2022-07-26 09:34:56 +0100

    build: :green_heart: Added setDefaultResultOrder to fix localhost

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import path from "path";
+import dns from 'dns'
+
+// https://vitejs.dev/config/server-options.html#server-host
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
   resolve: {
     alias: {
