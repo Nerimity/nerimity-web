c8bad10be67d33bbb2643da3e7e16a058efe8faf
Supertiger <saawalkhn@googlemail.com>
2022-09-18 11:36:55 +0100

    feat: :lipstick: Added mobile swiping drawers for mobile layout

--- src/components/ui/drawer/index.tsx
+++ src/components/ui/drawer/index.tsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.scss'
 import { useWindowProperties } from '@/common/useWindowProperties';
 import {createEffect, createSignal, JSX, on, onCleanup, onMount} from 'solid-js';
+import env from '@/common/env';
 
 interface DrawerLayoutProps {
   LeftDrawer: () => JSX.Element;
@@ -16,14 +17,48 @@ export default function DrawerLayout(props: DrawerLayoutProps) {
 
   let containerEl: HTMLDivElement | undefined = undefined;
   const [startPos, setStartPos] = createSignal({x: 0, y: 0});
-  const [xTransform, setXTransform] = createSignal(0);
+  const [startTransformX, setStartTransformX] = createSignal(0);
+  const [transformX, setTransformX] = createSignal(0);
   const [currentPage, setCurrentPage] = createSignal(1);
   let startTime = 0;
 
   const {width} = useWindowProperties();
 
+  const isMobile = () => width() <= env.MOBILE_WIDTH;
+
   
   
+  createEffect(on(isMobile, () => {
+    if (isMobile()) {
+      addEvents();
+      updatePage();
+    }
+    if (!isMobile()) {
+      removeEvents();
+      setTransformX(0);
+    }
+
+
+    onCleanup(() => {
+      removeEvents();
+    })
+  }))
+
+  const addEvents = () => {
+    window.addEventListener("touchstart", onTouchStart);
+    window.addEventListener("touchmove", onTouchMove);
+    window.addEventListener("touchend", onTouchEnd);
+    window.addEventListener("scroll", onScroll, true);
+  }
+  const removeEvents = () => {
+    window.removeEventListener("touchstart", onTouchStart);
+    window.removeEventListener("touchmove", onTouchMove);
+    window.removeEventListener("touchend", onTouchEnd);
+    window.removeEventListener("scroll", onScroll);
+  }
+
+
+
   
   const drawerWidth = () => {
     const dWidth = width() - 60;
@@ -33,10 +68,18 @@ export default function DrawerLayout(props: DrawerLayoutProps) {
   };
   const totalWidth = () => (drawerWidth() * 2) + width() 
 
+  let velocityTimeout: any;
   const updatePage = () => {
-    if (currentPage() === 0) setXTransform(0);
-    if (currentPage() === 1) setXTransform(setXTransform(-drawerWidth()));
-    if (currentPage() === 2) setXTransform(-totalWidth() - -width());
+    if (!isMobile()) return;
+    velocityTimeout && clearTimeout(velocityTimeout);
+
+    containerEl!.style.transition = `translate 0.2s`
+    velocityTimeout = setTimeout(() => {
+      containerEl!.style.transition = "";
+    }, 200)  
+    if (currentPage() === 0) setTransformX(0);
+    if (currentPage() === 1) setTransformX(-drawerWidth());
+    if (currentPage() === 2) setTransformX(-totalWidth() - -width());
   }
 
   createEffect(on(currentPage, () => {
@@ -44,29 +87,44 @@ export default function DrawerLayout(props: DrawerLayoutProps) {
   }))
 
   const onTouchStart = (event: TouchEvent) => {
+    setStartTransformX(transformX());
     const x = event.touches[0].clientX;
     const y = event.touches[0].clientY;
-    setStartPos({x: x - xTransform(), y});
+    setStartPos({x: x - transformX(), y});
     startTime = Date.now();
   }
   const onTouchMove = (event: TouchEvent) => {
     const x = event.touches[0].clientX;
     const touchDistance = x - startPos().x;
+
+    if (currentPage() === 0 && -touchDistance >= drawerWidth() ) {
+      return setTransformX(-drawerWidth());
+    }
+
+
+    if (currentPage() === 2 && -touchDistance <= drawerWidth()) {
+      return setTransformX(-drawerWidth());
+    }
     if (touchDistance >=0) {
       setStartPos({...startPos(), x});
-      return setXTransform(0);
+      return setTransformX(0);
     }
     if (touchDistance <= -totalWidth() + width() ) {
-
-      setStartPos({...startPos(), x: x - xTransform()});
-      return setXTransform(-totalWidth() + width());
+      setStartPos({...startPos(), x: x - transformX()});
+      return setTransformX(-totalWidth() + width());
     }
-    setXTransform(touchDistance);
+
+
+
+
+    setTransformX(touchDistance);
   }
   const onTouchEnd = (event: TouchEvent) => {
-    const isOnLeftDrawer = xTransform() - -drawerWidth() >= drawerWidth() /2;
-    const isOnRightDrawer = xTransform() - -(totalWidth() - width())<= drawerWidth() /2;
+    const isOnLeftDrawer = transformX() - -drawerWidth() >= drawerWidth() /2;
+    const isOnRightDrawer = transformX() - -(totalWidth() - width())<= drawerWidth() /2;
     const isOnContent = !isOnLeftDrawer && !isOnRightDrawer;
+
+    const beforePage = currentPage();
     
     
     if (isOnLeftDrawer) setCurrentPage(0);
@@ -74,7 +132,25 @@ export default function DrawerLayout(props: DrawerLayoutProps) {
     if (isOnRightDrawer) setCurrentPage(2);
 
 
-    // console.log(xTransform(),  startPos().x)
+    const distance = startTransformX() - transformX();
+    const time = Date.now() - startTime;
+    const velocity = Math.abs(distance / time);
+
+
+    if (time <= 150 && velocity >= 0.5) {
+      const isSwipingLeft = distance <=0;
+      const isSwipingRight = distance >=1;
+
+      if (isSwipingRight && beforePage <= 2) {
+        setCurrentPage(beforePage+1);
+      } else if (isSwipingLeft && beforePage >=0) {
+        setCurrentPage(beforePage-1);
+      }
+    }
+
+
+
+    updatePage();
 
 
   }
@@ -82,26 +158,14 @@ export default function DrawerLayout(props: DrawerLayoutProps) {
 
   }
 
-  onMount(() => {
-    window.addEventListener("touchstart", onTouchStart);
-    window.addEventListener("touchmove", onTouchMove);
-    window.addEventListener("touchend", onTouchEnd);
-    window.addEventListener("scroll", onScroll, true);
-    onCleanup(() => {
-      window.removeEventListener("touchstart", onTouchStart);
-      window.removeEventListener("touchmove", onTouchMove);
-      window.removeEventListener("touchend", onTouchEnd);
-      window.removeEventListener("scroll", onScroll);
-    })
-  })
 
 
   return (
     <div class={styles.drawerLayout}>
-      <div ref={containerEl} class={styles.container}  style={{translate: xTransform() + "px"}}>
-        <div style={{width: drawerWidth() + "px", display: 'flex'}}><props.LeftDrawer/></div>
-        <div class={styles.content} style={{width: width() + "px"}}><props.Content/></div>
-        <div style={{width: drawerWidth() + "px", display: 'flex'}}><props.RightDrawer/></div>
+      <div ref={containerEl} class={styles.container}  style={{translate: transformX() + "px"}}>
+        <div style={{width: drawerWidth() + "px", display: 'flex', "flex-shrink": 0}}><props.LeftDrawer/></div>
+        <div class={styles.content} style={{width: isMobile() ? width() + "px" : '100%'}}><props.Content/></div>
+        <div style={{width: isMobile() ? drawerWidth() + "px" : '250px', display: 'flex', "flex-shrink": 0}}><props.RightDrawer/></div>
       </div>
     </div>
   )
