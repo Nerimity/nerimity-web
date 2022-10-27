53b673a3e3b2801f1e04995189c6830137266a92
Supertiger <saawalkhn@googlemail.com>
2022-10-27 21:58:51 +0100

    Change document title using env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { onMount } from 'solid-js';
 import { RouterView } from 'solid-named-router';
+import env from './common/env';
 import { isHalloween } from './worldEvents';
 
 export default function App() {
 
 
   onMount(() => {
+    document.title = env.APP_NAME
     if (isHalloween) {
       document.documentElement.style.setProperty('--primary-color', '#df6f1a');
     }
