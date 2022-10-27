f7699bfb576d12fda5dad0bed06b1bbd2777ca18
Supertiger <saawalkhn@googlemail.com>
2022-10-27 17:05:25 +0100

    added spooky color ?

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { onMount } from 'solid-js';
 import { RouterView } from 'solid-named-router';
 
 export default function App() {
+  const halloweenStart = 1666652400000;
+  const halloweenEnd = 1667343600000;
+  const now = Date.now();
+
+  onMount(() => {
+    // check if now is between halloweenStart and halloweenEnd
+    if (now > halloweenStart && now < halloweenEnd) {
+      document.documentElement.style.setProperty('--primary-color', '#df6f1a');
+    }
+  })
 
   return (
     <RouterView />
