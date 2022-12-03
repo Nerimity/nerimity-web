074b7fbc994c274ad55d495cd2afcd16d4a1a9aa
Supertiger <saawalkhn@googlemail.com>
2022-12-03 08:46:45 +0000

    style: :lipstick: Adjusted modal size for changelog.

--- src/components/ChangelogModal.tsx
+++ src/components/ChangelogModal.tsx
@@ -1,6 +1,8 @@
 import { formatTimestamp } from "@/common/date";
 import Marked from "@/common/Marked";
 import { useAppVersion } from "@/common/useAppVersion";
+import { Link } from "@nerimity/solid-router";
+import Button from "./ui/Button";
 import { FlexColumn } from "./ui/Flexbox";
 import Modal from "./ui/Modal";
 import Text from "./ui/Text";
@@ -14,16 +16,23 @@ export function ChangelogModal (props: {close: () => void}) {
     return formatTimestamp(new Date(release.published_at).getTime())
   }
 
+  const ActionButtons = (
+    <Link style={{"text-decoration": "none"}} href="https://github.com/Nerimity/Nerimity-Web/releases" target="_blank" rel="noopener noreferrer">
+      <Button label="View more in GitHub" iconName="launch"  />
+    </Link>
+  )
+
+
   return (
-    <Modal title="Changelog" close={props.close}>
-    <FlexColumn gap={5}>
-      <FlexColumn style={{"max-height": "300px", "max-width": "500px", overflow: "auto"}}>
-        <Text size={24}>{latestRelease()?.name || ""}</Text>
-        <Text opacity={0.7}>Released at {date() || ""}</Text>
-        <Text opacity={0.7}>{latestRelease()?.tag_name}</Text>
-        <Marked value={latestRelease()?.body!} />
+    <Modal title="Changelog" close={props.close} actionButtons={ActionButtons} >
+      <FlexColumn gap={5}>
+        <FlexColumn style={{"max-height": "400px", "max-width": "900px", overflow: "auto"}}>
+          <Text size={24}>{latestRelease()?.name || ""}</Text>
+          <Text opacity={0.7}>Released at {date() || ""}</Text>
+          <Text opacity={0.7}>{latestRelease()?.tag_name}</Text>
+          <Marked value={latestRelease()?.body!} />
+        </FlexColumn>
       </FlexColumn>
-    </FlexColumn>
     </Modal>
   )
 }
