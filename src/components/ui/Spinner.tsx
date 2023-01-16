18fd0fa880bf7fa40e29e2f6eca5656f24738821
Supertigerr <saawalkhn@googlemail.com>
2023-01-16 15:42:02 +0000

    chore: :recycle: Updated spinner component

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,4 +1,6 @@
-export default function Spinner(props: {size?: number}) {
+import { JSX } from "solid-js";
+
+export default function Spinner(props: { style?: JSX.CSSProperties, size?: number}) {
   props.size = 80;
-  return <img height={props.size + "px"} width={props.size + "px"} src="/public/assets/spinner.svg"></img>
+  return <img style={props.style} height={props.size + "px"} width={props.size + "px"} src="/assets/spinner.svg"></img>
 }
