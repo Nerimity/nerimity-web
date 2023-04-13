2daff353fa5a8b3a70d9c1ed289c6ccce50e30b2
Supertigerr <saawalkhn@googlemail.com>
2023-04-13 19:58:59 +0100

    feat: :sparkles: Language settings can now include links to contributors.

--- src/components/settings/LanguageSettings.tsx
+++ src/components/settings/LanguageSettings.tsx
@@ -1,16 +1,17 @@
-import { createEffect, createSignal, For} from 'solid-js';
+import { createEffect, createSignal, For, Show} from 'solid-js';
 import Text from '@/components/ui/Text';
 import { css, styled } from 'solid-styled-components';
 import { getCurrentLanguage, getLanguage, Language, languages, setCurrentLanguage } from '@/locales/languages';
 
 import ItemContainer from '../ui/Item';
 import twemoji from 'twemoji';
-import { FlexColumn } from '../ui/Flexbox';
+import { FlexColumn, FlexRow } from '../ui/Flexbox';
 import useStore from '@/chat-api/store/useStore';
 import { useTransContext } from '@nerimity/solid-i18next';
 import env from '@/common/env';
 import { emojiUnicodeToShortcode, unicodeToTwemojiUrl } from '@/emoji';
 import { Emoji } from '../markup/Emoji';
+import { CustomLink } from '../ui/CustomLink';
 
 const Container = styled("div")`
   display: flex;
@@ -23,7 +24,9 @@ const LanguageItemContainer = styled(ItemContainer)`
   padding: 5px;
   gap: 10px;
   padding-left: 10px;
-  box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.4);
+  &:hover {
+    box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.4);
+  }
 `;
 
 export default function LanguageSettings() {
@@ -74,9 +77,43 @@ function LanguageItem(props: {key: string, selected: boolean, onClick: () => voi
       <Emoji class={css`height: 30px; width: 30px;`}  name={emojiUnicodeToShortcode(language.emoji)} url={unicodeToTwemojiUrl(language.emoji)} />
       <FlexColumn>
         <Text>{language.name}</Text>
-        <Text size={12} opacity={0.7}>Contributors: {language.contributors.join(", ")}</Text>
+        <Contributors contributors={language.contributors} />
       </FlexColumn>
     </LanguageItemContainer>
   )
 }
 
+const ContributorContainer = styled(FlexRow)`
+  font-size: 14px;
+`;
+
+function Contributors(props: {contributors: string[]}) {
+  return (
+    <FlexRow>
+      <Text size={14} style={{"margin-right": "5px"}}>Contributors:</Text>
+      <For each={props.contributors}>
+        {(contributor, i) => (
+          <ContributorContainer gap={5}>
+            <Show when={i() > 0}>{", "}</Show>
+            <Show when={isUrl(contributor)}><CustomLink decoration href={contributor} target="_blank" rel="noopener noreferrer">{lastPath(contributor)}</CustomLink></Show>
+            <Show when={!isUrl(contributor)}><Text size={14} opacity={0.8}>{contributor}</Text></Show>
+          </ContributorContainer>
+        )}
+      </For>
+    </FlexRow>
+  )
+}
+
+function isUrl(url: string) {
+  try {
+    new URL(url);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function lastPath(url: string) {
+  const split = url.split('/');
+  return split[split.length - 1];
+}
