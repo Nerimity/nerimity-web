1f2481804b2154fe856e00950b1268c9425bf29e
Supertigerr <saawalkhn@googlemail.com>
2023-05-01 10:30:46 +0100

    feat: :sparkles: Added pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import solidPlugin from 'vite-plugin-solid';
 import path from "path";
 import dns from 'dns'
 
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/server-options.html#server-host
 dns.setDefaultResultOrder('verbatim')
+
 
 export default defineConfig({
   resolve: {
@@ -12,7 +14,37 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     }
   },
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({ 
+      manifest: {
+        name: 'Nerimity',
+        short_name: 'Nerimity',
+        theme_color: '#77a8f3',
+        description: 'A modern and sleek chat app.',
+        id: '/app',
+        start_url: '/app',
+        background_color: 'hsl(216deg 9% 8%)',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png', // <== don't add slash, for testing
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png', // <== don't remove slash, for testing
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: false,
+      }
+    })
+  ],
   build: {
     target: 'esnext',
   },
