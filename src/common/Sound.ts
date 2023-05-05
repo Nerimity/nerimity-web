43e9c032cf816284acdb8206147b778c8571dd55
Supertigerr <saawalkhn@googlemail.com>
2023-05-05 11:00:10 +0100

    fix: :bug: When in Do not disturb, do not play notification sounds.

--- src/common/Sound.ts
+++ src/common/Sound.ts
@@ -1,6 +1,8 @@
+import useAccount from "@/chat-api/store/useAccount";
 import { getStorageBoolean, StorageKeys } from "./localStorage";
 import { useWindowProperties } from "./useWindowProperties";
-
+import useStore from "@/chat-api/store/useStore";
+import { UserStatus } from "@/chat-api/store/useUsers";
 export const MESSAGE_NOTIFICATION = "message-notification.mp3";
 
 export function playSound(name: string) {
@@ -12,6 +14,11 @@ export function playSound(name: string) {
 
 
 export function playMessageNotification() {
+  const {account, users} = useStore();
   if (getStorageBoolean(StorageKeys.ARE_NOTIFICATIONS_MUTED, false)) return;
+  const userId = account.user()?.id;
+  const user = users.get(userId!);
+  if (user?.presence?.status === UserStatus.DND) return;
+
   playSound(MESSAGE_NOTIFICATION);
 }
