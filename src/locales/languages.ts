8daba83c3a17b6ea641cdaf3a55c8483eadc76aa
Supertiger <saawalkhn@googlemail.com>
2023-07-18 09:21:22 +0100

    Fixed json

--- src/locales/languages.ts
+++ src/locales/languages.ts
@@ -35,11 +35,11 @@ export const languages = {
     name: "Polish",
     emoji: "🇵🇱",
     contributors: ["https://github.com/Ciach0", "https://github.com/CyberL1"],
-  }
+  },
   "de-de": {
     name: "German",
     emoji: "🇩🇪",
-    contributors: [https://github.com/Tycraft2005/],
+    contributors: ["https://github.com/Tycraft2005/"],
   }
 }
 
