66ca2f12af6a7eb2a3529e611b69b500a122ee3f
Supertiger <saawalkhn@googlemail.com>
2024-04-01 12:52:06 +0100

    Fix custom link not showing warning modal.

--- src/components/markup/Link.tsx
+++ src/components/markup/Link.tsx
@@ -1,24 +1,14 @@
 import { CustomLink } from "../ui/CustomLink";
-import { DangerousLinkModal } from "../ui/DangerousLinkModal";
-import { useCustomPortal } from "../ui/custom-portal/CustomPortal";
 
 export function Link(props: { url: string, text?: string; }) {
-  const {createPortal} = useCustomPortal();
   const safeUrl = () => {
     const startsWithHttp = props.url.startsWith("http://");
     const startsWithHttps = props.url.startsWith("https://");
     if (startsWithHttp || startsWithHttps) return props.url;
     return `https://${props.url}`;
   };
-  const onClick = (event: MouseEvent) => {
-    event.stopPropagation();
-    if (props.text === undefined) return;
-    if (props.text === safeUrl()) return; 
-    event?.preventDefault();
-    createPortal(close => <DangerousLinkModal unsafeUrl={props.url} close={close} />);
-  };
 
   return (
-    <CustomLink decoration href={safeUrl()} target="_blank" rel="noopener noreferrer" onClick={onClick}>{props.text || safeUrl()}</CustomLink>
+    <CustomLink decoration isDangerous={!!props.text && props.text !== safeUrl()} href={safeUrl()} target="_blank" rel="noopener noreferrer">{props.text || safeUrl()}</CustomLink>
   );
 }
