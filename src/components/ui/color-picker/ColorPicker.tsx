e635f974e6d8c02631456f7d908cffc1b816fcdb
Supertiger <saawalkhn@googlemail.com>
2024-04-16 15:32:25 +0100

    Cleaned up code

--- src/components/ui/color-picker/ColorPicker.tsx
+++ src/components/ui/color-picker/ColorPicker.tsx
@@ -2,8 +2,8 @@ import Icon from "../icon/Icon";
 import styles from "./styles.module.scss";
 
 import "@melloware/coloris/dist/coloris.css";
-import { coloris, init, close, OnChangeCallback, updatePosition } from "@melloware/coloris";
-import { Show, createEffect, createSignal, on, onMount } from "solid-js";
+import { coloris, init, updatePosition } from "@melloware/coloris";
+import { createEffect, on, onMount } from "solid-js";
 import Modal from "../modal/Modal";
 import { useCustomPortal } from "../custom-portal/CustomPortal";
 import { useWindowProperties } from "@/common/useWindowProperties";
@@ -12,10 +12,7 @@ import { classNames, conditionalClass } from "@/common/classNames";
 init();
 
 export function ColorPicker(props: { color: string | null, onChange?: (value: string) => void }) {
-  let inputEl: undefined | HTMLInputElement;
-  const [opened, setOpened] = createSignal(false);
   const {createPortal} = useCustomPortal();
-  
   
 
   let timeout: null | number = null;
@@ -29,14 +26,6 @@ export function ColorPicker(props: { color: string | null, onChange?: (value: st
 
   const onClicked = () => {
     createPortal?.(close => <ColorPickerModal color={props.color} close={close} onChange={onChange} />);
-    
-    // if (opened()) {
-    //   setOpened(false);
-    //   return;
-    // }
-    // setOpened(true);
-    // coloris({el: inputEl!, themeMode: "dark", parent: "#coloris", defaultColor: props.color || "black", inline: true, onChange});
-
   };
 
   return (
@@ -45,7 +34,6 @@ export function ColorPicker(props: { color: string | null, onChange?: (value: st
       <div class={styles.colorPicker} style={{ background: props.color || "transparent" }} onClick={onClicked}>
 
         <Icon name='colorize' color='white' size={18} class={styles.icon} />
-        <input style={{ position: "absolute", opacity: 0 }} ref={inputEl} value={props.color || "transparent"} />
       </div>
     </>
   );
