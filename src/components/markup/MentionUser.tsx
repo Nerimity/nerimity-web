b478687ec5ab8b0aea57ceb0be3bdd4a8e9f4613
Supertiger <saawalkhn@googlemail.com>
2024-05-27 09:37:29 +0100

    Show profile popup when clicking on a mention

--- src/components/markup/MentionUser.tsx
+++ src/components/markup/MentionUser.tsx
@@ -1,31 +1,44 @@
 import { RawUser } from "@/chat-api/RawData";
-import { avatarUrl, User } from "@/chat-api/store/useUsers";
 import RouterEndpoints from "@/common/RouterEndpoints";
 import Avatar from "../ui/Avatar";
-import { CustomLink } from "../ui/CustomLink";
 import { createSignal, Show } from "solid-js";
-import { useParams } from "solid-navigator";
+import { A, useParams } from "solid-navigator";
 import MemberContextMenu from "../member-context-menu/MemberContextMenu";
+import { useCustomPortal } from "../ui/custom-portal/CustomPortal";
+import { ProfileFlyout } from "../floating-profile/FloatingProfile";
 
 export function MentionUser(props: { user: RawUser }) {
   const [contextPosition, setContextPosition] = createSignal<{ x: number, y: number } | undefined>(undefined);
+  const { createPortal } = useCustomPortal();
+
   const params = useParams<{ serverId?: string }>();
+
   const onContext = (event: MouseEvent) => {
     event.preventDefault();
     event.stopPropagation();
     setContextPosition({ x: event.clientX, y: event.clientY });
   };
 
+  const showProfileFlyout = (event: MouseEvent) => {
+    event.preventDefault();
+    const el = event.target as HTMLElement;
+    const rect = el?.getBoundingClientRect()!;
+    const pos = { left: rect.left + 40, top: rect.top, anchor: "left" } as const;
+    return createPortal(close => <ProfileFlyout triggerEl={el} position={pos} serverId={params.serverId} close={close} userId={props.user.id} />, "profile-pane-flyout-" + props.user.id, true);
+  };
+
+
   return (
-    <CustomLink
+    <A
+      onClick={showProfileFlyout}
       onContextMenu={onContext}
       href={RouterEndpoints.PROFILE(props.user.id)}
-      class="mention">
+      class="mention trigger-profile-flyout">
       <Avatar class="avatar" user={props.user} size={16} />
       {props.user.username}
       <Show when={contextPosition()}>
         <MemberContextMenu user={props.user} position={contextPosition()} serverId={params.serverId} userId={props.user.id} onClose={() => setContextPosition(undefined)} />
       </Show>
-    </CustomLink>
+    </A>
   );
 }
