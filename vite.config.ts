04667310575df34fce23bf9a4b1723f51342b316
Supertiger <saawalkhn@googlemail.com>
2024-07-20 08:22:50 +0100

    Updated packages & add autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import path from "path";
 import dns from "dns";
-import Icons from 'unplugin-icons/vite';
+import Icons from "unplugin-icons/vite";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/server-options.html#server-host
 dns.setDefaultResultOrder("verbatim");
@@ -10,20 +11,22 @@ dns.setDefaultResultOrder("verbatim");
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-  plugins: [
-    solidPlugin(),
-    Icons({ compiler: 'solid', autoInstall: true }),
-  ],
+  plugins: [solidPlugin(), Icons({ compiler: "solid", autoInstall: true })],
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
   build: {
     target: "esnext",
-    sourcemap: true
+    sourcemap: true,
   },
   server: {
     host: true,
     port: 3000,
-    open: true
-  }
+    open: true,
+  },
 });
