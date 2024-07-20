dc819468e51660b85f7af5e9b08897633cdcedb1
Supertiger <saawalkhn@googlemail.com>
2024-07-20 12:02:22 +0100

    Hugely decreased bundle size

--- src/common/zip.ts
+++ src/common/zip.ts
@@ -1,26 +1,30 @@
-import pako from "pako";
-import * as Base64 from "base64-arraybuffer";
-
-export function zip(value: string) {
-  const binaryString = pako.deflate(value);
-  return Base64.encode(binaryString);
-}
+import uzip from "uzip";
 
 export function unzip(base64: string) {
   try {
-    return pako.inflate(Base64.decode(base64), { to: "string" });
-  }
-  catch {
+    return uzip.inflate(base64ToArrayBuffer(base64));
+  } catch {
     return null;
   }
 }
 
-
-export function unzipJson(base64: string){
+export function unzipJson(base64: string) {
   try {
-    return JSON.parse(unzip(base64) || "");
-  }
-  catch {
+    return JSON.parse(decodeU8intoUTF8(unzip(base64)!) || "");
+  } catch {
     return null;
   }
+}
+
+function base64ToArrayBuffer(base64: string) {
+  var binaryString = atob(base64);
+  var bytes = new Uint8Array(binaryString.length);
+  for (var i = 0; i < binaryString.length; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes;
+}
+
+function decodeU8intoUTF8(bytes: Uint8Array) {
+  return new TextDecoder("utf-8").decode(bytes);
 }
