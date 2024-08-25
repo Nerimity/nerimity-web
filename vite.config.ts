2686624d7a2027be7b71ab068f08e8f27b252906
QuizzityMC <158294486+QuizzityMC@users.noreply.github.com>
2024-08-26 07:56:54 +1000

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import path from "path";
+import dns from "dns";
+import autoprefixer from "autoprefixer";
+
+// https://vitejs.dev/config/server-options.html#server-host
+dns.setDefaultResultOrder("verbatim");
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [solidPlugin()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
+  build: {
+    target: "esnext",
+    sourcemap: true,
+  },
+  server: {
+    host: true,
+    port: 80,
+    open: true,
+    proxy: {
+      "/cdn": {
+        target: "http://localhost:8003",
+        rewrite: (path) => path.replace(/^\/cdn/,""),
+      },
+    },
+  },
+});
