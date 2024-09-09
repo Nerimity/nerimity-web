990710da1caf961d888a37fd1ac85683e967db4d
Supertiger <saawalkhn@googlemail.com>
2024-09-09 08:48:31 +0100

    customize header color

--- src/common/themes.ts
+++ src/common/themes.ts
@@ -2,6 +2,8 @@ import { StorageKeys, useReactiveLocalStorage } from "./localStorage";
 
 export const theme = {
   "background-color": "hsl(216deg 9% 8%)",
+  "header-background-color": "rgba(48, 48, 48, 0.86)",
+  "header-background-color-blur-disabled": "rgb(48, 48, 48)",
   "primary-color": "#4c93ff",
   "alert-color": "#eb6e6e",
   "warn-color": "#e8a859",
@@ -10,31 +12,35 @@ export const theme = {
   "success-color-dark": "#1c221d",
   "primary-color-dark": "#2d3746",
   "alert-color-dark": "#3e2626",
-  "warn-color-dark": "#3a3229"
+  "warn-color-dark": "#3a3229",
 };
 
 type ThemeKey = keyof typeof theme;
 
-const [customColors, setCustomColors] = useReactiveLocalStorage<Partial<Record<ThemeKey, string>>>(StorageKeys.CUSTOM_COLORS, {});
+const [customColors, setCustomColors] = useReactiveLocalStorage<
+  Partial<Record<ThemeKey, string>>
+>(StorageKeys.CUSTOM_COLORS, {});
 
-const currentTheme = () => ({...theme, ...customColors()});
+const currentTheme = () => ({ ...theme, ...customColors() });
 
 export const updateTheme = () => {
   const newTheme = currentTheme();
 
   for (const key in newTheme) {
-    document.documentElement.style.setProperty(`--${key}`, newTheme[key as ThemeKey]);
-  }  
+    document.documentElement.style.setProperty(
+      `--${key}`,
+      newTheme[key as ThemeKey]
+    );
+  }
 };
 
 export const setThemeColor = (key: string, value?: string) => {
   if (value === undefined) {
-    const temp = {...customColors()};
+    const temp = { ...customColors() };
     delete temp[key];
     setCustomColors(temp);
-  }
-  else {
-    setCustomColors({...customColors(), [key]: value});
+  } else {
+    setCustomColors({ ...customColors(), [key]: value });
   }
   updateTheme();
 };
