a82d7ddacec8a64dffb09e16015241ced7c62c17
Supertiger <saawalkhn@googlemail.com>
2025-04-07 11:31:15 +0100

    update mod pane

--- src/components/moderation-pane/ModerationPane.tsx
+++ src/components/moderation-pane/ModerationPane.tsx
@@ -624,18 +624,6 @@ export function User(props: { user: any; class?: string }) {
               Banned
             </Text>
           </Show>
-          <Show when={props.user._count?.devices}>
-            <Text
-              size={12}
-              style={{
-                background: "var(--alert-color)",
-                "border-radius": "4px",
-                padding: "3px",
-              }}
-            >
-              IP Banned
-            </Text>
-          </Show>
           <Show when={props.user.shadowBan}>
             <Text
               size={12}
