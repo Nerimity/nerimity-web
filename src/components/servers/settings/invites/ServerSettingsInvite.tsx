3e3413bb415661cb0aa9e98a929a1950d2d26f62
Supertiger <saawalkhn@googlemail.com>
2025-05-17 12:21:09 +0100

    fix bug with creating invites

--- src/components/servers/settings/invites/ServerSettingsInvite.tsx
+++ src/components/servers/settings/invites/ServerSettingsInvite.tsx
@@ -1,6 +1,11 @@
 import styles from "./styles.module.scss";
 import RouterEndpoints from "@/common/RouterEndpoints";
-import { createCustomInvite, createInvite, deleteInvite, getInvites } from "@/chat-api/services/ServerService";
+import {
+  createCustomInvite,
+  createInvite,
+  deleteInvite,
+  getInvites,
+} from "@/chat-api/services/ServerService";
 import Avatar from "@/components/ui/Avatar";
 import Button from "@/components/ui/Button";
 import env from "@/common/env";
@@ -40,66 +45,90 @@ export default function ServerSettingsInvite() {
     setInvites(invites);
   };
 
-  createEffect(on(() => params.serverId, () => {
-    fetchInvites();
-  }));
-
+  createEffect(
+    on(
+      () => params.serverId,
+      () => {
+        fetchInvites();
+      }
+    )
+  );
 
   createEffect(() => {
-
     header.updateHeader({
       title: "Settings - Invites",
       serverId: params.serverId!,
-      iconName: "settings"
+      iconName: "settings",
     });
   });
 
   const isServerOwner = () => server()?.createdById === account.user()?.id;
 
-
   const onCreateInviteClick = async () => {
     await createInvite(params.serverId!);
-    getInvites(params.serverId!).then((invites) => setInvites(invites.reverse()));
+    fetchInvites();
   };
 
   const deleteInvite = (code: string) => {
-    setInvites(invites().filter(i => i.code !== code));
+    setInvites(invites().filter((i) => i.code !== code));
   };
 
   return (
-    <div class={classNames(styles.invitesPane, conditionalClass(mobileSize(), styles.mobile))}>
+    <div
+      class={classNames(
+        styles.invitesPane,
+        conditionalClass(mobileSize(), styles.mobile)
+      )}
+    >
       <Breadcrumb>
-        <BreadcrumbItem href={RouterEndpoints.SERVER_MESSAGES(params.serverId, server()?.defaultChannelId!)} icon='home' title={server()?.name} />
+        <BreadcrumbItem
+          href={RouterEndpoints.SERVER_MESSAGES(
+            params.serverId,
+            server()?.defaultChannelId!
+          )}
+          icon="home"
+          title={server()?.name}
+        />
         <BreadcrumbItem title={t("servers.settings.drawer.invites")} />
       </Breadcrumb>
-      <Show when={isServerOwner()}><CustomInvite invites={invites()} onUpdate={fetchInvites} /></Show>
+      <Show when={isServerOwner()}>
+        <CustomInvite invites={invites()} onUpdate={fetchInvites} />
+      </Show>
 
-
-      <SettingsBlock label={t("servers.settings.invites.serverInvites")} description={t("servers.settings.invites.serverInvitesDescription")} icon='mail' header={true}>
-        <Button label={t("servers.settings.invites.createInviteButton")} onClick={onCreateInviteClick} />
+      <SettingsBlock
+        label={t("servers.settings.invites.serverInvites")}
+        description={t("servers.settings.invites.serverInvitesDescription")}
+        icon="mail"
+        header={true}
+      >
+        <Button
+          label={t("servers.settings.invites.createInviteButton")}
+          onClick={onCreateInviteClick}
+        />
       </SettingsBlock>
       <For each={invites()}>
         {(invite) => (
-          <InviteItem invite={invite} onDeleted={() => deleteInvite(invite.code)} />
+          <InviteItem
+            invite={invite}
+            onDeleted={() => deleteInvite(invite.code)}
+          />
         )}
       </For>
     </div>
   );
 }
 
-
-
-function CustomInvite(props: { invites: any[]; onUpdate: () => void; }) {
+function CustomInvite(props: { invites: any[]; onUpdate: () => void }) {
   const [t] = useTransContext();
   const params = useParams<{ serverId: string }>();
   const { servers } = useStore();
   const server = () => servers.get(params.serverId);
-  const prefixUrl = env.APP_URL + RouterEndpoints.EXPLORE_SERVER_INVITE_SHORT("");
+  const prefixUrl =
+    env.APP_URL + RouterEndpoints.EXPLORE_SERVER_INVITE_SHORT("");
 
   const [error, setError] = createSignal<null | string>(null);
 
-
-  const customInvite = () => props.invites.find(invite => invite.isCustom);
+  const customInvite = () => props.invites.find((invite) => invite.isCustom);
   const [customCode, setCustomCode] = createSignal("");
 
   createEffect(() => {
@@ -109,44 +138,87 @@ function CustomInvite(props: { invites: any[]; onUpdate: () => void; }) {
   const showCustomCodeSaveButton = () => {
     if (!customInvite() && !customCode().trim().length) return false;
     if (!customInvite() && customCode().trim().length) return true;
-    if (customInvite() && customCode().trim() === customInvite().code) return false;
+    if (customInvite() && customCode().trim() === customInvite().code)
+      return false;
     return true;
   };
 
   const createInvite = () => {
     setError(null);
-    createCustomInvite(customCode().trim(), params.serverId).then(invite => {
-      setCustomCode(invite.code);
-      props.onUpdate();
-    }).catch(err => {
-      setError(err.message);
-    });
+    createCustomInvite(customCode().trim(), params.serverId)
+      .then((invite) => {
+        setCustomCode(invite.code);
+        props.onUpdate();
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
   };
 
   return (
     <FlexColumn style={{ "margin-bottom": "10px" }}>
       <Show when={!server()?.verified}>
-        <Notice class={css`margin-bottom: 10px;`} type='info' description={t("servers.settings.invites.customInviteVerifiedOnlyNotice")} />
+        <Notice
+          class={css`
+            margin-bottom: 10px;
+          `}
+          type="info"
+          description={t(
+            "servers.settings.invites.customInviteVerifiedOnlyNotice"
+          )}
+        />
       </Show>
 
-      <SettingsBlock class={css`&&{position: relative; overflow: hidden; ${!server()?.verified ? "cursor: not-allowed; opacity: 0.6;" : ""} }`} label={t("servers.settings.invites.customLink")} icon='link'>
+      <SettingsBlock
+        class={css`
+          && {
+            position: relative;
+            overflow: hidden;
+            ${!server()?.verified ? "cursor: not-allowed; opacity: 0.6;" : ""}
+          }
+        `}
+        label={t("servers.settings.invites.customLink")}
+        icon="link"
+      >
         {/* Overlay to block actions when server is not verified. */}
         <Show when={!server()?.verified}>
           <div style={{ position: "absolute", inset: 0, "z-index": 1111 }} />
         </Show>
-        <Input prefix={prefixUrl} onText={t => setCustomCode(t)} value={customCode()} />
+        <Input
+          prefix={prefixUrl}
+          onText={(t) => setCustomCode(t)}
+          value={customCode()}
+        />
       </SettingsBlock>
-      <Show when={error()}><Text style={{ "align-self": "end" }} size={12} color="var(--alert-color)">{error()}</Text></Show>
-      <Show when={showCustomCodeSaveButton()} ><Button onClick={createInvite} class={css`align-self: self-end; margin-bottom: -8px;`} label={t("servers.settings.invites.saveButton")} iconName='save' /></Show>
+      <Show when={error()}>
+        <Text
+          style={{ "align-self": "end" }}
+          size={12}
+          color="var(--alert-color)"
+        >
+          {error()}
+        </Text>
+      </Show>
+      <Show when={showCustomCodeSaveButton()}>
+        <Button
+          onClick={createInvite}
+          class={css`
+            align-self: self-end;
+            margin-bottom: -8px;
+          `}
+          label={t("servers.settings.invites.saveButton")}
+          iconName="save"
+        />
+      </Show>
     </FlexColumn>
   );
 }
 
-
-
-const InviteItem = (props: { invite: any, onDeleted: () => void }) => {
+const InviteItem = (props: { invite: any; onDeleted: () => void }) => {
   const [t] = useTransContext();
-  const url = env.APP_URL + RouterEndpoints.EXPLORE_SERVER_INVITE_SHORT(props.invite.code);
+  const url =
+    env.APP_URL +
+    RouterEndpoints.EXPLORE_SERVER_INVITE_SHORT(props.invite.code);
 
   const onDeleteClick = async () => {
     await deleteInvite(props.invite.serverId, props.invite.code);
@@ -158,18 +230,37 @@ const InviteItem = (props: { invite: any, onDeleted: () => void }) => {
       <Avatar class={styles.avatar} user={props.invite.createdBy} size={30} />
       <div class={styles.detailsOuter}>
         <div class={styles.details}>
-          <A href={RouterEndpoints.EXPLORE_SERVER_INVITE_SHORT(props.invite.code)} class={styles.url}>{url}</A>
+          <A
+            href={RouterEndpoints.EXPLORE_SERVER_INVITE_SHORT(
+              props.invite.code
+            )}
+            class={styles.url}
+          >
+            {url}
+          </A>
           <div class={styles.otherDetails}>
-            <Icon name='person' size={14} class={styles.icon} />
+            <Icon name="person" size={14} class={styles.icon} />
             {props.invite.createdBy.username}
-            <Icon name='whatshot' size={14} class={styles.icon} />
+            <Icon name="whatshot" size={14} class={styles.icon} />
             {props.invite.uses} uses
-            <Icon name='today' size={14} class={styles.icon} />
-            {formatTimestamp(props.invite.createdAt)}</div>
+            <Icon name="today" size={14} class={styles.icon} />
+            {formatTimestamp(props.invite.createdAt)}
+          </div>
         </div>
         <FlexRow class={styles.buttons}>
-          <Button onClick={() => copyToClipboard(url)} class={classNames(styles.copyButton, styles.button)} label={t("servers.settings.invites.copyLinkButton")} iconName='content_copy' />
-          <Button onClick={onDeleteClick} class={classNames(styles.deleteButton, styles.button)} label={t("servers.settings.invites.deleteButton")} iconName='delete' color='var(--alert-color)' />
+          <Button
+            onClick={() => copyToClipboard(url)}
+            class={classNames(styles.copyButton, styles.button)}
+            label={t("servers.settings.invites.copyLinkButton")}
+            iconName="content_copy"
+          />
+          <Button
+            onClick={onDeleteClick}
+            class={classNames(styles.deleteButton, styles.button)}
+            label={t("servers.settings.invites.deleteButton")}
+            iconName="delete"
+            color="var(--alert-color)"
+          />
         </FlexRow>
       </div>
     </div>
