516745d41855a5df6db5fc3c6f0f52c27b848b8d
SupertigerDev <saawalkhn@googlemail.com>
2025-10-07 21:07:22 +0100

    fix title not updating

--- src/common/MetaTitle.tsx
+++ src/common/MetaTitle.tsx
@@ -1,11 +1,11 @@
 import { Title } from "@solidjs/meta";
-import { children, createEffect, JSXElement } from "solid-js";
+import { children, JSXElement } from "solid-js";
 import env from "./env";
 
 export const MetaTitle = (props: { children: JSXElement }) => {
   const el = children(() => props.children);
-  const text = el.toArray().join(" ");
-  const full = `${text || ""} - Nerimity ${env.DEV_MODE ? "DEV" : ""}`;
+  const text = () => el.toArray().join(" ");
+  const full = () => `${text() || ""} - Nerimity ${env.DEV_MODE ? "DEV" : ""}`;
 
-  return <Title>{full}</Title>;
+  return <Title>{full()}</Title>;
 };
