66fa20acbb5cca136bb272d4c14a1c80ac79de88
Asraye <asrayeofficial@gmail.com>
2025-11-23 03:10:43 +1100

    i18n: add friend modal

--- src/components/home-drawer/add-friend/AddFriendModal.tsx
+++ src/components/home-drawer/add-friend/AddFriendModal.tsx
@@ -1,36 +1,40 @@
 import style from "./styles.module.scss";
-
 import Input from "@/components/ui/input/Input";
 import { Modal } from "@/components/ui/modal";
 import { useAddFriendModalController } from "./AddFriendModalController";
+import { t } from "@nerimity/i18lite";
 
 export default function AddFriendModal(props: { close: () => void }) {
   const controller = useAddFriendModalController();
 
   return (
     <Modal.Root close={props.close}>
-      <Modal.Header title="Add Friend" icon="group_add" />
+      <Modal.Header title={t("addFriendModal.title")} icon="group_add" />
       <Modal.Body class={style.body}>
         <div class={style.description}>
-          Enter your friends username and tag to add them.
+          {t("addFriendModal.description")}
         </div>
         <Input
-          placeholder="Username:ABC1"
+          placeholder={t("addFriendModal.placeholder")}
           error={controller.error().message}
           onText={controller.setUserTag}
           value={controller.userTag()}
-          success={controller.success() && "Request sent!"}
+          success={controller.success() && t("addFriendModal.requestSent")}
         />
       </Modal.Body>
       <Modal.Footer>
         <Modal.Button
-          label="Close"
+          label={t("addFriendModal.closeButton")}
           alert
           iconName="close"
           onClick={props.close}
         />
         <Modal.Button
-          label={controller.requestSent() ? "Sending..." : "Send Request"}
+          label={
+            controller.requestSent()
+              ? t("addFriendModal.sending")
+              : t("addFriendModal.sendRequest")
+          }
           primary
           iconName="add"
           onClick={controller.onSendClick}
