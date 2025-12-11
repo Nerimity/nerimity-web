7ac09a21f3a4c3bdc303fa60a805a07814b67958
Asraye <asrayeofficial@gmail.com>
2025-12-12 03:31:38 +1100

    feat: handle post links to open nicely

--- src/components/ui/CustomLink.tsx
+++ src/components/ui/CustomLink.tsx
@@ -1,5 +1,5 @@
 import { classNames, conditionalClass } from "@/common/classNames";
-import { A } from "solid-navigator";
+import { A, useSearchParams } from "solid-navigator";
 import { css } from "solid-styled-components";
 import { useCustomPortal } from "./custom-portal/CustomPortal";
 import { DangerousLinkModal } from "./DangerousLinkModal";
@@ -25,8 +25,12 @@ const decoration = css`
   }
 `;
 
+const POST_LINK_REGEX = /^https?:\/\/nerimity\.com\/p\/(\d+)$/i;
+
 export function CustomLink(props: CustomLinkProps) {
   const { createPortal } = useCustomPortal();
+  const [, setSearchParams] = useSearchParams();
+
   const onContextMenu = (event: MouseEvent) => {
     props.onContextMenu?.(event);
     if (!props.noContextMenu) return;
@@ -34,11 +38,27 @@ export function CustomLink(props: CustomLinkProps) {
   };
 
   const onLinkClick = (e: MouseEvent) => {
-    if (!props.isDangerous) return props.onClick?.(e) || props.onclick?.(e);
-    e.preventDefault();
-    createPortal((close) => (
-      <DangerousLinkModal unsafeUrl={props.href || "#"} close={close} />
-    ));
+    if (props.isDangerous) {
+      e.preventDefault();
+      createPortal((close) => (
+        <DangerousLinkModal unsafeUrl={href || "#"} close={close} />
+      ));
+      return;
+    }
+
+    const href = props.href;
+    const match = href?.match(POST_LINK_REGEX);
+    const postId = match ? match[1] : undefined;
+
+    if (postId) {
+      e.preventDefault();
+      // Open post
+      setSearchParams({ postId });
+
+      return;
+    }
+
+    return props.onClick?.(e) || props.onclick?.(e);
   };
 
   return (
