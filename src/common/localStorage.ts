ad919b28c8d57a4d1e0808657683cf9d6235d09c
Asraye <asrayeofficial@gmail.com>
2025-12-15 20:41:35 +1100

    chore: add timeFormat into StorageKeys

--- src/common/localStorage.ts
+++ src/common/localStorage.ts
@@ -33,6 +33,7 @@ export const StorageKeys = {
   HIDDEN_ANNOUNCEMENT_IDS: "hiddenAnnouncementIds",
   CHAT_BAR_OPTIONS: "chatBarOptions",
   MENTION_REPLIES: "mentionReplies",
+  TIME_FORMAT: "timeFormat", 
 } as const;
 
 export type StorageKeys = (typeof StorageKeys)[keyof typeof StorageKeys];
