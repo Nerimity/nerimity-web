b93edd9dc1cba398cce83937632a787a033a8509
Asraye <asrayeofficial@gmail.com>
2025-12-24 03:09:46 +1100

    feat: move openInviteBotModal to own file

---
+++ src/components/ui/openInviteBotModal.tsx
@@ -0,0 +1,32 @@
+import { css } from "solid-styled-components";
+import { Modal } from "./modal";
+import { InviteBotPopup } from "@/pages/InviteServerBot";
+import { useCustomPortal } from "./custom-portal/CustomPortal";
+
+export const openInviteBotModal = (
+  createPortal: ReturnType<typeof useCustomPortal>["createPortal"],
+  appId: string,
+  permissions?: number
+) => {
+  createPortal((c) => (
+    <Modal.Root
+      close={c}
+      desktopMaxWidth={400}
+      desktopClass={css`
+        width: 100%;
+      `}
+    >
+      <Modal.Header title="Invite Bot" />
+      <Modal.Body
+        class={css`
+          overflow: auto;
+        `}
+      >
+        <InviteBotPopup
+          appId={appId}
+          permissions={permissions}
+        />
+      </Modal.Body>
+    </Modal.Root>
+  ));
+};
