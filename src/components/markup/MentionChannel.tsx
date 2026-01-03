671adcb6be0cb3bad0bf25bcf2e1c63e4fa2db66
Asraye <asrayeofficial@gmail.com>
2026-01-04 02:23:27 +1100

    fix: check if admin/owner when mentioned channel

--- src/components/markup/MentionChannel.tsx
+++ src/components/markup/MentionChannel.tsx
@@ -2,13 +2,24 @@ import { Show } from "solid-js";
 import { Channel } from "@/chat-api/store/useChannels";
 import RouterEndpoints from "@/common/RouterEndpoints";
 import { CustomLink } from "../ui/CustomLink";
-import { CHANNEL_PERMISSIONS } from "@/chat-api/Bitwise";
+import { CHANNEL_PERMISSIONS, ROLE_PERMISSIONS } from "@/chat-api/Bitwise";
 import Icon from "../ui/icon/Icon";
 import { t } from "@nerimity/i18lite";
+import useServerMembers from "@/chat-api/store/useServerMembers"; 
+import useAccount from "@/chat-api/store/useAccount"; 
 
 export function MentionChannel(props: {channel: Channel}) {
-  const canView = () => 
-    props.channel && props.channel.hasPermission(CHANNEL_PERMISSIONS.PUBLIC_CHANNEL);
+  const serverMembers = useServerMembers();
+  const account = useAccount();
+
+  const canView = () => {
+    const channel = props.channel;
+    if (!channel || !channel.serverId) return false;
+    const member = serverMembers.get(channel.serverId, account.user()?.id!);
+    const hasAdminPerm = member?.hasPermission(ROLE_PERMISSIONS.ADMIN);
+    if (hasAdminPerm) return true;
+    return channel.hasPermission(CHANNEL_PERMISSIONS.PUBLIC_CHANNEL);
+  };
 
   return (
     <Show 
