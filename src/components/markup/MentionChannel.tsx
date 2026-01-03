a6b9deec904ebb417c51f2e34e9e2cf7a7df63d3
Asraye <asrayeofficial@gmail.com>
2026-01-03 23:05:12 +1100

    feat: render hidden channel mentions differently

--- src/components/markup/MentionChannel.tsx
+++ src/components/markup/MentionChannel.tsx
@@ -1,14 +1,53 @@
+import { Show } from "solid-js";
 import { Channel } from "@/chat-api/store/useChannels";
 import RouterEndpoints from "@/common/RouterEndpoints";
 import { CustomLink } from "../ui/CustomLink";
+import { CHANNEL_PERMISSIONS } from "@/chat-api/Bitwise";
+import Icon from "../ui/icon/Icon";
+import { t } from "@nerimity/i18lite";
 
 export function MentionChannel(props: {channel: Channel}) {
+  const canView = () => 
+    props.channel && props.channel.hasPermission(CHANNEL_PERMISSIONS.PUBLIC_CHANNEL, true);
 
   return (
-    <CustomLink 
-      href={RouterEndpoints.SERVER_MESSAGES(props.channel.serverId!, props.channel.id)} 
-      class="mention">
-      <span class="type">#</span>{props.channel.name}
-    </CustomLink>
+    <Show 
+      when={canView()} 
+      fallback={
+        <span 
+          class="mention hidden-mention" 
+          style={{ 
+            display: "inline-flex", 
+            "align-items": "center", 
+            gap: "4px",
+            position: "relative",
+            cursor: "not-allowed",
+            "vertical-align": "middle"
+          }}
+        >
+          <div style={{ 
+            position: "absolute", 
+            inset: 0, 
+            "z-index": 1, 
+            "background-color": "transparent" 
+          }} />
+          <Icon 
+            name="lock" 
+            size={14} 
+            style={{ color: "var(--primary)" }} 
+          />
+          <span style={{ "white-space": "nowrap" }}>
+            {t("servers.channelPermissions.hiddenChannel")}
+          </span>
+        </span>
+      }
+    >
+      <CustomLink 
+        href={RouterEndpoints.SERVER_MESSAGES(props.channel!.serverId!, props.channel!.id)} 
+        class="mention"
+      >
+        <span class="type">#</span>{props.channel!.name}
+      </CustomLink>
+    </Show>
   );
 }
