1cbe82b12ebacb6012411f10e5759617318507d2
Supertiger <saawalkhn@googlemail.com>
2026-01-13 09:56:11 +0000

    change logout modal header color

--- src/components/settings/LogoutModal.tsx
+++ src/components/settings/LogoutModal.tsx
@@ -7,7 +7,7 @@ export const LogoutModal = (props: { close: () => void }) => {
   const [clearLocalSettings, setClearLocalSettings] = createSignal(true);
   return (
     <Modal.Root close={props.close}>
-      <Modal.Header title="Logout?" icon="logout" />
+      <Modal.Header title="Logout?" icon="logout" alert />
       <Modal.Body>Are you sure you want to logout?</Modal.Body>
       <Checkbox
         label="Clear Local Settings"
