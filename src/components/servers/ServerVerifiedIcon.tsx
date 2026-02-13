7d445a9bd34f21ce72a3c2d74607524dc480a194
lenify <lenify@tutanota.com>
2026-02-13 16:03:50 +0300

    Add strings for codeblocks and server verified icon

--- src/components/servers/ServerVerifiedIcon.tsx
+++ src/components/servers/ServerVerifiedIcon.tsx
@@ -2,12 +2,13 @@ import { css } from "solid-styled-components";
 import Icon from "../ui/icon/Icon";
 import { className } from "solid-js/web";
 import { classNames } from "@/common/classNames";
+import { t } from "@nerimity/i18lite";
 
 export function ServerVerifiedIcon (props: {class?: string}) {
   return (
     <Icon 
       class={classNames(css`user-select: none;`, props.class)}
-      title='This server is verified.'
+      title={t("channelDrawer.verified")}
       name='verified'
       color='var(--primary-color)' size={16}
     />
