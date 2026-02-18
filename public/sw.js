cca38a91accf100154e5703b0f9a461c202b7791
SupertigerDev <saawalkhn@googlemail.com>
2026-02-18 22:27:50 +0000

    refactor: optimize fetch event handling in service worker

--- public/sw.js
+++ public/sw.js
@@ -10,25 +10,28 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", async (event) => {
   if (event.request.method !== "GET") return;
 
   const url = new URL(event.request.url);
   const isSameOrigin = url.origin === location.origin;
   const isAssetsRequest = isSameOrigin && url.pathname.startsWith("/assets/");
 
-  const cacheFirst = async (request, shouldWrite) => {
-    const cache = await caches.open(CACHE_NAME);
-    const cached = await cache.match(request);
-    if (cached) console.log(`Cache hit for ${request.url}`);
-    if (cached) return cached;
+  event.respondWith(
+    (async () => {
+      if (!isAssetsRequest) {
+        return fetch(event.request);
+      }
 
-    const response = await fetch(request);
-    if (shouldWrite && response && response.ok) {
-      cache.put(request, response.clone());
-    }
-    return response;
-  };
+      const cache = await caches.open(CACHE_NAME);
+      const cached = await cache.match(event.request);
+      if (cached) return cached;
 
-  event.respondWith(cacheFirst(event.request, isAssetsRequest));
+      const response = await fetch(event.request);
+      if (response && response.ok) {
+        cache.put(event.request, response.clone());
+      }
+      return response;
+    })()
+  );
 });
