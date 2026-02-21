4324c5da4009efedbcd1d0baac57c8b9483aac3b
Aeledfyr <aeledfyr@gmail.com>
2026-02-20 23:40:18 -0800

    feat: use newer invite embed in custom emoji info

--- src/components/markup/Emoji.tsx
+++ src/components/markup/Emoji.tsx
@@ -2,18 +2,10 @@ import { classNames } from "@/common/classNames";
 import { useWindowProperties } from "@/common/useWindowProperties";
 import { useCustomPortal } from "../ui/custom-portal/CustomPortal";
 import LegacyModal from "../ui/legacy-modal/LegacyModal";
-import { css, styled } from "solid-styled-components";
+import { styled } from "solid-styled-components";
 import Text from "../ui/Text";
-import { Show, createEffect, createSignal, onMount } from "solid-js";
-import { publicServerByEmojiId } from "@/chat-api/services/ServerService";
-import { RawExploreItem } from "@/chat-api/RawData";
-import Avatar from "../ui/Avatar";
-import Button from "../ui/Button";
-import useStore from "@/chat-api/store/useStore";
-import { ServerVerifiedIcon } from "../servers/ServerVerifiedIcon";
-import { useNavigate } from "solid-navigator";
-import RouterEndpoints from "@/common/RouterEndpoints";
-import { useJoinServer } from "@/chat-api/useJoinServer";
+import { Show, createSignal } from "solid-js";
+import { ServerInviteEmbed } from "../message-pane/message-item/MessageItem";
 
 export function Emoji(props: {
   clickable?: boolean;
@@ -69,7 +61,7 @@ const EmojiDetailsContainer = styled.div`
   gap: 8px;
   position: relative;
   align-self: center;
-  min-width: 200px;
+  min-width: 260px;
 
   margin: 10px;
 `;
@@ -101,16 +93,9 @@ function EmojiDetailsModal(props: {
   custom?: boolean;
   id?: string;
 }) {
-  const [publicServer, setPublicServer] = createSignal<RawExploreItem | null>(
-    null
-  );
   const { shouldAnimate } = useWindowProperties();
   const [hovered, setHovered] = createSignal(false);
 
-  onMount(() => {
-    if (!props.custom || !props.id) return;
-    publicServerByEmojiId(props.id).then(setPublicServer);
-  });
   return (
     <LegacyModal
       close={props.close}
@@ -149,101 +134,12 @@ function EmojiDetailsModal(props: {
           </EmojiNameContainer>
         </MainEmojiContainer>
 
-        <Show when={props.custom}>
-          <PublicServer
-            close={props.close}
-            publicServer={publicServer()!}
-            emojiId={props.id!}
+        <Show when={props.custom && props.id}>
+          <ServerInviteEmbed
+            emojiId={props.id}
           />
         </Show>
       </EmojiDetailsContainer>
     </LegacyModal>
-  );
-}
-
-const PublicServerContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  background-color: rgba(0, 0, 0, 0.2);
-  padding: 6px;
-  padding-left: 12px;
-  padding-right: 6px;
-  border-radius: 6px;
-`;
-
-const PublicServerDetailsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 4px;
-`;
-
-const PublicServerNameContainer = styled.span`
-  display: inline;
-  vertical-align: 1.3;
-`;
-
-function PublicServer(props: {
-  publicServer?: RawExploreItem;
-  emojiId?: string;
-  close: () => void;
-}) {
-  const [hovered, setHovered] = createSignal(false);
-  const { joinPublicById, joining } = useJoinServer();
-  const navigate = useNavigate();
-  const { servers } = useStore();
-
-  const serverId = () => {
-    if (props.publicServer) return props.publicServer.server?.id;
-    const emoji = servers
-      .emojisUpdatedDupName()
-      .find((e) => e.id === props.emojiId);
-    return emoji?.serverId;
-  };
-
-  const server = () => props.publicServer?.server! || servers.get(serverId()!);
-  const isInServer = () => servers.get(serverId()!);
-
-  const joinOrVisitServer = () => {
-    if (isInServer())
-      return navigate(
-        RouterEndpoints.SERVER_MESSAGES(server().id, server().defaultChannelId)
-      );
-
-    if (joining()) return;
-
-    joinPublicById(serverId()!);
-  };
-
-  return (
-    <Show when={server()}>
-      <PublicServerContainer
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-      >
-        <Avatar animate={hovered()} size={40} server={server()} />
-        <PublicServerDetailsContainer>
-          <PublicServerNameContainer>
-            <Text size={14}>{server().name}</Text>
-            <Show when={props.publicServer?.server?.verified}>
-              {" "}
-              <ServerVerifiedIcon
-                class={css`
-                  vertical-align: -3px;
-                `}
-              />
-            </Show>
-          </PublicServerNameContainer>
-          <Button
-            onClick={joinOrVisitServer}
-            styles={{ "align-self": "flex-start" }}
-            iconName="login"
-            label={isInServer() ? "Visit Server" : "Join Server"}
-            margin={0}
-          />
-        </PublicServerDetailsContainer>
-      </PublicServerContainer>
-    </Show>
   );
 }
