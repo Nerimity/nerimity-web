0b2e0b16d2027709e2329fa85c96f48b57388a01
lenify <le@ni.fy>
2026-02-21 22:48:43 +0300

    Make Privacy Policy and TOS pages the same style

--- src/pages/PrivacyPage.tsx
+++ src/pages/PrivacyPage.tsx
@@ -49,7 +49,7 @@ export default function PrivacyPage() {
         <CenterContainer>
           <InformationContainer>
             <Title>Nerimity Privacy Policy</Title>
-            <Text opacity={0.6} size={14}>
+            <Text opacity={0.8} size={14}>
               Last updated: 20 February 2026
             </Text>
             <CustomLink
@@ -60,11 +60,13 @@ export default function PrivacyPage() {
             >
               View policy history
             </CustomLink>
-            <Text opacity={0.8} size={16}>
-              If you find something missing or have any questions, email us at
-              nerimityapp@gmail.com.
+            <Text opacity={0.6} size={14}>
+              If you have questions or notice something missing, you may email
+              us at{" "}
+              <a href="mailto:nerimityapp@gmail.com">nerimityapp@gmail.com</a>.
             </Text>
           </InformationContainer>
+
           <Block title="Data We Store Automatically" icon="storage">
             When registering an account on Nerimity and using its services, we
             store some information. The information we store may include but not
@@ -161,7 +163,11 @@ const TitleWithIcon = styled("div")`
   margin-bottom: 10px;
 `;
 
-function Block(props: { title: string; icon?: string; children: JSXElement }) {
+function Block(props: {
+  title: string;
+  icon?: string;
+  children: JSXElement
+}) {
   return (
     <BlockContainer>
       <TitleWithIcon>
