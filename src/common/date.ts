ffa1b2d2b2f57b08eacd283fdd92923b28b3504c
Aeledfyr <aeledfyr@gmail.com>
2026-02-23 18:46:29 -0800

    feat: set up Temporal polyfill

--- src/common/date.ts
+++ src/common/date.ts
@@ -2,6 +2,8 @@ import { useLocalStorage, StorageKeys } from "@/common/localStorage";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
 dayjs.extend(duration);
+import { getCurrentLanguageISO } from "@/locales/languages";
+import { Temporal, Intl } from "temporal-polyfill";
 
 export const [timeFormat, setTimeFormat] = useLocalStorage<"12hr" | "24hr">(
    StorageKeys.TIME_FORMAT,
