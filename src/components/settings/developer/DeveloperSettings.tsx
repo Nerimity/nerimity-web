1543771d89e8698ba628349ff5c2ad0ef36ac737
mathiiiiiis <mathis@mathiiis.de>
2026-03-03 19:16:16 +0100

    fix: use existing checkbox component

--- src/components/settings/developer/DeveloperSettings.tsx
+++ src/components/settings/developer/DeveloperSettings.tsx
@@ -7,6 +7,7 @@ import { t } from "@nerimity/i18lite";
 import Breadcrumb, { BreadcrumbItem } from "@/components/ui/Breadcrumb";
 import SettingsBlock from "@/components/ui/settings-block/SettingsBlock";
 import Icon from "@/components/ui/icon/Icon";
+import Checkbox from "@/components/ui/Checkbox";
 
 const Container = styled("div")`
   display: flex;
@@ -25,10 +26,8 @@ export default function DeveloperSettings() {
 
   const [useLatest, setUseLatest] = createSignal(getInitialCookieValue());
 
-  const handleToggleCookie = (e: Event) => {
-    const checked = (e.target as HTMLInputElement).checked;
+  const handleToggleCookie = (checked: boolean) => {
     setUseLatest(checked);
-    
     const domain = window.location.hostname.includes("nerimity.com") ? "domain=.nerimity.com;" : "";
     document.cookie = `useLatestURL=${checked}; path=/; max-age=315360000; ${domain}`;
   };
@@ -52,13 +51,7 @@ export default function DeveloperSettings() {
         label={t("settings.developer.useLatestUrl")}
         description={<>{t("settings.developer.useLatestUrlDescription")} <span style={{ "font-weight": "800" }}>{t("settings.developer.useLatestUrlWarning")}</span></>}
         children={
-          <input
-            type="checkbox"
-            aria-label={t("settings.developer.useLatestUrl")}
-            checked={useLatest()}
-            onChange={handleToggleCookie}
-            style={{ cursor: "pointer", width: "20px", height: "20px", margin: "0 8px" }}
-          />
+          <Checkbox checked={useLatest()} onChange={handleToggleCookie} />
         }
       />
 
