058158bc3651c34e8a8c7666149e87fc0c17e36e
Supertiger <saawalkhn@googlemail.com>
2026-03-20 08:32:10 +0000

    fix roles not showing

--- src/components/floating-profile/FloatingProfile.tsx
+++ src/components/floating-profile/FloatingProfile.tsx
@@ -523,20 +523,9 @@ const DesktopProfileFlyout = (props: {
   const ProfileArea = () => {
     const memberRoles = serverMembers.roles(member()!, true) || [];
 
-    const showRoles = () => {
-      if (!server()) return false;
-      if (memberRoles.length === 0) return false;
-      const hasPerm = serverMembers.hasPermission(
-        accountMember()!,
-        ROLE_PERMISSIONS.MANAGE_ROLES
-      );
-      if (!hasPerm) return false;
-      return true;
-    };
-
     return (
       <>
-        <Show when={showRoles()}>
+        <Show when={server()}>
           <div class={styles.section}>
             <FlyoutTitle
               primaryColor={colors()?.primary || undefined}
